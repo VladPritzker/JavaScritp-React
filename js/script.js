b42dd2aad1af6073717a713fd2c770b57311bb22
“vlad” <pritzkervlad@gmail.com>
2022-12-24 11:04:08 -0500

    secont commit

--- js/script.js
+++ js/script.js
@@ -9,3 +9,30 @@ console.log(`https://someurl.com/${category}/5`);
 
 const user = "Vlad the best creature in the world";
 alert(`Hello, ${user}`);
+
+//Операторы 
+console.log('arr' + " - object");
+
+console.log(4 + +'фкк')
+// ёто выведет результат NaN так как, так как мы не можем добвавить число к строке
+console.log(4 + +'5')
+// ёто выведет результат 9 
+
+let incr = 10,
+decr = 10;
+incr++;
+incr--;
+
+console.log(incr)
+console.log(decr)
+
+
+
+////
+const Closed = true,
+	isOpen = false;
+console.log( Closed && !isOpen) // выдаст результат true, так как open равнялось fals,
+
+
+
+
