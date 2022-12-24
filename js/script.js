72647e067b5102941d934f18cf38c3cf1fed87ff
“vlad” <pritzkervlad@gmail.com>
2022-12-24 10:57:08 -0500

    Firstcommit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const answers = []
+answers[0] = prompt('What is your name?', '')
+answers[1] = prompt('What is your lastname?', '')
+answers[2] = prompt('What is your DOB?', '')
+console.log(answers)
+
+const category = 'toys'
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Vlad the best creature in the world";
+alert(`Hello, ${user}`);
